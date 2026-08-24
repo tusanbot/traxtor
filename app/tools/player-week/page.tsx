fab2f5874c412455d0996608213ee29a4a2dcbff
tusanbot <mshmahyar9@gmail.com>
2026-08-24 14:04:52 +0330

    feat: add player of the week route

---
+++ app/tools/player-week/page.tsx
@@ -0,0 +1,5 @@
+import PlayerWeekTool from "@/components/player-week/PlayerWeekTool";
+
+export const metadata = { title: "بازیکن هفته | Traxtor", description: "ساخت طرح گرافیکی بازیکن هفته" };
+
+export default function PlayerWeekPage() { return <PlayerWeekTool />; }
