469aa0aafbbafe3b510a05c206129157222919b6
tusanbot <mshmahyar9@gmail.com>
2026-08-24 13:57:29 +0330

    feat: add team stats route

---
+++ app/tools/team-stats/page.tsx
@@ -0,0 +1,10 @@
+import TeamStatsTool from "@/components/team-stats/TeamStatsTool";
+
+export const metadata = {
+  title: "آمار تیم | Traxtor",
+  description: "ساخت طرح گرافیکی آمار تیم فوتبال",
+};
+
+export default function TeamStatsPage() {
+  return <TeamStatsTool />;
+}
