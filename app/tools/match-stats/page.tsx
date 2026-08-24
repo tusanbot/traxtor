c1157b3213692871af19d3c42d93b86d6018bfed
tusanbot <mshmahyar9@gmail.com>
2026-08-24 14:11:01 +0330

    feat: add match stats route

---
+++ app/tools/match-stats/page.tsx
@@ -0,0 +1,3 @@
+import MatchStatsTool from "@/components/match-stats/MatchStatsTool";
+export const metadata={title:"آمار بازی | Traxtor",description:"ساخت طرح گرافیکی آمار مسابقه فوتبال"};
+export default function MatchStatsPage(){return <MatchStatsTool/>}
