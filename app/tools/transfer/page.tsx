10b54ccf7fd851f610cd308c464f840f5c1772dc
tusanbot <mshmahyar9@gmail.com>
2026-08-24 14:03:13 +0330

    feat: add transfer route

---
+++ app/tools/transfer/page.tsx
@@ -0,0 +1,5 @@
+import TransferTool from "@/components/transfer/TransferTool";
+
+export const metadata = { title: "طرح نقل‌وانتقال | Traxtor", description: "ساخت طرح گرافیکی نقل‌وانتقالات فوتبال" };
+
+export default function TransferPage() { return <TransferTool />; }
