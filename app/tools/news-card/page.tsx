610c9b79156769a3a0188de9d35867490fbdf198
tusanbot <mshmahyar9@gmail.com>
2026-08-24 13:59:39 +0330

    feat: add news card route

---
+++ app/tools/news-card/page.tsx
@@ -0,0 +1,10 @@
+import NewsCardTool from "@/components/news-card/NewsCardTool";
+
+export const metadata = {
+  title: "طرح خبر | Traxtor",
+  description: "ساخت کارت گرافیکی اخبار فوتبال",
+};
+
+export default function NewsCardPage() {
+  return <NewsCardTool />;
+}
