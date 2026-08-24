904a6b0f2436ab7ec1adac0cc5ab3b9f06966cd4
tusanbot <mshmahyar9@gmail.com>
2026-08-24 12:27:45 +0330

    feat: add match result route

---
+++ app/tools/match-result/page.tsx
@@ -0,0 +1,10 @@
+import MatchResultTool from "@/components/match-result/MatchResultTool";
+
+export const metadata = {
+  title: "نتیجه بازی | Traxtor",
+  description: "ساخت طرح گرافیکی نتیجه مسابقه فوتبال",
+};
+
+export default function MatchResultPage() {
+  return <MatchResultTool />;
+}
