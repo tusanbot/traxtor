6fcca01dba0d683a70c64ebd93ca9e7e4677862e
tusanbot <mshmahyar9@gmail.com>
2026-08-24 14:01:31 +0330

    feat: add league table route

---
+++ app/tools/league-table/page.tsx
@@ -0,0 +1,10 @@
+import LeagueTableTool from "@/components/league-table/LeagueTableTool";
+
+export const metadata = {
+  title: "جدول لیگ | Traxtor",
+  description: "ساخت طرح گرافیکی جدول لیگ فوتبال",
+};
+
+export default function LeagueTablePage() {
+  return <LeagueTableTool />;
+}
