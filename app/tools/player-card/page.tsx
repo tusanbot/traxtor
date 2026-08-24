2ae22e49a50d5f4224414ea027247596e3e6de8a
tusanbot <mshmahyar9@gmail.com>
2026-08-24 10:01:17 +0330

    feat: add player card page

---
+++ app/tools/player-card/page.tsx
@@ -0,0 +1,10 @@
+import PlayerCardTool from "@/components/player-card/PlayerCardTool";
+
+export const metadata = {
+  title: "کارت بازیکن | Traxtor",
+  description: "ساخت کارت گرافیکی بازیکن برای فن‌پیج تراکتور",
+};
+
+export default function PlayerCardPage() {
+  return <PlayerCardTool />;
+}
