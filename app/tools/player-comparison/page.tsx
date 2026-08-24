e0d2c21afc2a63cbb9878109223c75c4a916f203
tusanbot <mshmahyar9@gmail.com>
2026-08-24 14:23:05 +0330

    feat: add player comparison page

---
+++ app/tools/player-comparison/page.tsx
@@ -0,0 +1,3 @@
+import PlayerComparisonTool from "@/components/player-comparison/PlayerComparisonTool";
+export const metadata={title:"مقایسه بازیکنان | Traxtor",description:"ساخت طرح گرافیکی مقایسه دو بازیکن"};
+export default function PlayerComparisonPage(){return <PlayerComparisonTool/>;}
